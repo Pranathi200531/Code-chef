1ab1c6ad44a2a6c40996a351570569c23d9ba271
Gnanakoushik <jpranathi04@gmail.com>
2024-12-06 06:57:20 +0530

    Add files via upload

---
+++ Code1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Code1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int d=sc.nextInt();
+		if(d==25)
+		System.out.println("CHRISTMAS");
+		else
+		System.out.println("ORDINARY");
+
+	}
+}
