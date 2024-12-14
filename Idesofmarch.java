708316cc264eeae4ef9181bc2c3df5690de3e348
Pranathi200531 <jpranathi04@gmail.com>
2024-12-14 15:47:57 +0530

    Add files via upload

---
+++ Idesofmarch.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Idesofmarch
+
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	if(n<15)
+	System.out.println("NO");
+	else
+	System.out.println("YES");
+
+
+	}
+}
