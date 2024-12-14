ece89aaf3d18454d796a36c529701e3eadbc2927
Pranathi200531 <jpranathi04@gmail.com>
2024-12-14 15:54:13 +0530

    Add files via upload

---
+++ SummerTime.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class SaummerTime
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n<=35)
+		System.out.println("NO");
+		else
+		System.out.println("YES");
+	}
+}
+
+
