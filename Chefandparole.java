74d3682ba12154647f32592e1cd215550990e7e4
Pranathi200531 <jpranathi04@gmail.com>
2024-12-14 15:56:25 +0530

    Add files via upload

---
+++ Chefandparole.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Chefandparole
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n<7)
+		System.out.println("No");
+		else
+		System.out.println("Yes");
+
+	}
+}
