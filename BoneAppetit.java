3f57e1d5263f6c5ce84b6f5f0a25794cca55b34f
Pranathi200531 <jpranathi04@gmail.com>
2025-01-08 22:56:39 +0530

    Add files via upload

---
+++ BoneAppetit.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class BoneAppetit
+{
+	public static void main (String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		int M=sc.nextInt();
+		int X=sc.nextInt();
+		int Y=sc.nextInt();
+		int c=N*X;
+		int a=M*Y;
+		System.out.println(c+a);
+		
+	}
+}
