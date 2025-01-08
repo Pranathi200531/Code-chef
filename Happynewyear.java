958299644876cee80ddfa234d493783b59ea0fce
Pranathi200531 <jpranathi04@gmail.com>
2025-01-08 23:03:29 +0530

    Add files via upload

---
+++ Happynewyear.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Happynewyear
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int X=sc.nextInt();
+	    System.out.println(24-X);
+
+	}
+}
