066517f1a361722fdb523c52b20980d9bed7534d
Pranathi200531 <jpranathi04@gmail.com>
2025-05-03 22:20:07 +0530

    Add files via upload

---
+++ IPL.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class IPL
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int X=sc.nextInt();
+		if(X>=7)
+		System.out.println("THALA");
+		else
+		System.out.println("BOOM");
+
+	}
+}
