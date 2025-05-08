47d68950d014547fbb78a779cd8dad81f0a2d606
Jettipalli Pranathi <jpranathi04@gmail.com>
2025-05-08 11:40:33 +0530

    Add files via upload

---
+++ PizzaSplit.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class PizzaSplit
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		int a=N%2;
+		if(a==0)
+		System.out.println("1");
+		else
+		System.out.println("2");
+
+	}
+}
