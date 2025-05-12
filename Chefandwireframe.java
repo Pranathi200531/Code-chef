9dabd1681bf2011a77b81e504d76e1788b73dca1
Jettipalli Pranathi <jpranathi04@gmail.com>
2025-05-12 21:34:45 +0530

    Add files via upload

---
+++ Chefandwireframe.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Chefandwireframe
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		while(T-->0)
+		{
+		    int N=sc.nextInt();
+		    int M=sc.nextInt();
+		    int X=sc.nextInt();
+		    System.out.println(((N*2)+(M*2))*X);
+            System.out.println("Thank u");
+		}
+	}
+}
