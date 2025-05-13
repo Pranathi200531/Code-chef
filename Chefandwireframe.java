c5c2ae6cd25f7781b4f65bef67d67d366688dbda
Jettipalli Pranathi <jpranathi04@gmail.com>
2025-05-13 21:27:40 +0530

    Add files via upload

--- Chefandwireframe.java
+++ Chefandwireframe.java
@@ -14,7 +14,54 @@ class Chefandwireframe
 		    int M=sc.nextInt();
 		    int X=sc.nextInt();
 		    System.out.println(((N*2)+(M*2))*X);
-            System.out.println("Thank u");
+            //import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		while(T-->0)
+		{
+		    int N=sc.nextInt();
+		    int M=sc.nextInt();
+		    int X=sc.nextInt();
+		    System.out.println(((N*2)+(M*2))*X);
+		}
+        import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		while(T-->0)
+		{
+		    int N=sc.nextInt();
+		    int M=sc.nextInt();
+		    int X=sc.nextInt();
+		    import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		while(T-->0)
+		{
+		    int N=sc.nextInt();
+		    int M=sc.nextInt();
+		    int X=sc.nextInt();
+		    System.out.println(((N*2)+(M*2))*X);
 		}
 	}
 }
