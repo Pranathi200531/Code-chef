cb660d8d636fa64a8fd49bb1d8206eb490ec85e0
Jettipalli Pranathi <jpranathi04@gmail.com>
2025-05-14 23:57:49 +0530

    Add files via upload

---
+++ MakesP1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class MakesP1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int A=sc.nextInt();
+		int B=sc.nextInt();
+		if(A<=B)
+		System.out.println("Tyro");
+		else
+		System.out.println("Dom");
+		
+
+	}
+}
