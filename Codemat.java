d4b6fe9c3e5bd5119220cbd9dbf7dade744e7e23
Jettipalli Pranathi <jpranathi04@gmail.com>
2025-05-14 23:55:25 +0530

    Add files via upload

---
+++ Codemat.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codemat
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner (System.in);
+		int T=sc.nextInt();
+		while(T-->0)
+		{
+		    int A=sc.nextInt();
+		    int B=sc.nextInt();
+		    if(A<=B)
+		    System.out.println("NO");
+		    else
+		    System.out.println("YES");
+		    
+		}
+
+	}
+}
