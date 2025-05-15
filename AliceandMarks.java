d7cfb469c7d6720b34db0d7b2a1b0bc93bc4d92c
Jettipalli Pranathi <jpranathi04@gmail.com>
2025-05-15 16:40:24 +0530

    Add files via upload

---
+++ AliceandMarks.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class AliceandMarks
+{
+	public static void main (String[] args)
+	{
+		//Scanner sc = new Scanner(System.in);
+		//int X = sc.nextInt();
+		//int Y = sc.nextInt();
+		
+	//	Scanner sc=new Scanner(System.in);
+	//	int x=sc.nextInt();
+	//	int Y=sc.nextInt();
+	//	if(X<=Y)
+	//	System.out.println("YES");
+	//	else
+	//	System.out.println("NO");
+		    Scanner sc=new Scanner (System.in);
+    int x=sc.nextInt();
+    int y=sc.nextInt();
+    if(x>=2*y){
+        System.out.println("YES");
+    }
+    else{System.out.println("NO");
+    }
+    }
+}
